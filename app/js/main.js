2fe06b2033890bbe229f825cffcb3fadedab201b
Josews <jjosews@gmail.com>
2022-07-08 21:08:51 +0500

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
 
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 });
