edac378cc104926a9468418bb68d95c3594dd550
Josews <jjosews@gmail.com>
2022-07-09 14:55:01 +0500

    added compliment + partners section

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,20 @@
 $(function(){
 
-  const swiper = new Swiper('.swiper', {
+  const swipercompliment = new Swiper('.swiper-compliment', {
+    loop: true,
+    slidesPerView: 3,
+    centeredSlides: true,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  const swiper = new Swiper('.swiper-slider', {
     loop: true,
     slidesPerView: 1,
     pagination: {
       el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
     },
   });
 
