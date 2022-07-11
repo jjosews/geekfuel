ea8b9d2bc4c9063d6fcc9f67bade6ef9d2589a96
Josews <jjosews@gmail.com>
2022-07-11 16:18:40 +0500

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,41 @@
 $(function(){
 
+  const burger = document?.querySelector('[data-burger]');
+  const nav = document?.querySelector('[data-nav]');
+  const navItems = nav?.querySelectorAll('.header__nav-item, .header__nav-login');
+  const body = document.body;
+  const header = document?.querySelector('.header');
+  const headerHeight = header.offsetHeight;
+  document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+
+  burger?.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll');
+    burger?.classList.toggle('burger--active');
+    nav?.classList.toggle('nav--visible');
+  });
+
+  navItems.forEach(el => {
+    el.addEventListener('click', () => {
+      body.classList.remove('stop-scroll');
+      burger?.classList.remove('burger--active');
+      nav?.classList.remove('nav--visible');
+    });
+  });
+
   const swipercompliment = new Swiper('.swiper-compliment', {
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     centeredSlides: true,
     grabCursor: true,
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
+    breakpoints: {
+      1201: {
+        slidesPerView: 3,
+      },
+    }
   });
 
   const swiper = new Swiper('.swiper-slider', {
@@ -15,6 +43,7 @@ $(function(){
     slidesPerView: 1,
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   });
 
